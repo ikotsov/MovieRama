be211c1a12dda34889009b709a65eda0dfaeadd4
ikotsov <i.kotsovilis@gmail.com>
2024-01-09 19:53:55 +0200

    add: lang config

--- config.js
+++ config.js
@@ -2,3 +2,5 @@ export const API_KEY = 'bc50218d91157b1ba4f142ef7baaa6a0';
 
 export const API_URL_MOVIE_LIST = 'https://api.themoviedb.org/3/movie/now_playing';
 export const API_URL_GENRE_LIST = 'https://api.themoviedb.org/3/genre/movie/list';
+
+export const LANGUAGE_CODE = 'en-US';
