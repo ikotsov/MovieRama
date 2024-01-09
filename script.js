ff069369d8d7c8bd2133b6dc152c10c1b6b119c4
ikotsov <i.kotsovilis@gmail.com>
2024-01-09 21:58:31 +0200

    add: app entry point

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+import { loadGenres, loadMovies, state } from './model.js';
+
+const app = async () => {
+  await loadGenres();
+  console.log('genres', state.genres);
+
+  await loadMovies(state.page);
+  console.log('movies', state.movies);
+};
+app();
