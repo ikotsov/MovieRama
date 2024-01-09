6fb43f790c9aaf7a7524a4b452cddaa75146dfd6
ikotsov <i.kotsovilis@gmail.com>
2024-01-09 19:27:58 +0200

    add: network config

---
+++ config.js
@@ -0,0 +1,4 @@
+export const API_KEY = 'bc50218d91157b1ba4f142ef7baaa6a0';
+
+export const API_URL_MOVIE_LIST = 'https://api.themoviedb.org/3/movie/now_playing';
+export const API_URL_GENRE_LIST = 'https://api.themoviedb.org/3/genre/movie/list';
