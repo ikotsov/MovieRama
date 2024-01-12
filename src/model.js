8402b17c22ca2403fd260d26e627f37dacb2fedd
ikotsov <i.kotsovilis@gmail.com>
2024-01-12 16:58:55 +0200

    add: search results loader

--- src/model.js
+++ src/model.js
@@ -2,6 +2,7 @@ import {
   API_KEY,
   API_URL_GENRE_LIST,
   API_URL_MOVIE_LIST,
+  URL_MOVIE_SEARCH,
   LANGUAGE_CODE,
 } from './config.js';
 import { fetchJsonData } from './helpers.js';
@@ -64,6 +65,26 @@ export const loadGenres = async () => {
   try {
     const data = await fetchJsonData(`${API_URL_GENRE_LIST}?${params}`);
     state.genres = data.genres;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const createSearchResultsParams = (query, page) => {
+  const params = new URLSearchParams();
+  params.append('api_key', API_KEY);
+  params.append('page', page);
+  params.append('language', LANGUAGE_CODE);
+  params.append('query', query);
+  return params;
+};
+
+export const loadSearchResults = async (query) => {
+  const params = createSearchResultsParams(query, ++state.search.page);
+  try {
+    const data = await fetchJsonData(`${URL_MOVIE_SEARCH}?${params}`);
+    const newResults = data.results.map(createMovie);
+    state.search.results = [...state.search.results, ...newResults];
   } catch (error) {
     throw error;
   }
