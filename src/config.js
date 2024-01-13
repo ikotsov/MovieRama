d727494c5c28969e9209fb04657836344ad26dfb
ikotsov <i.kotsovilis@gmail.com>
2024-01-13 19:12:00 +0200

    refactor: extract a base url

--- src/config.js
+++ src/config.js
@@ -1,13 +1,15 @@
 export const API_KEY = 'bc50218d91157b1ba4f142ef7baaa6a0';
 
-export const API_URL_MOVIE_LIST = 'https://api.themoviedb.org/3/movie/now_playing';
-export const API_URL_GENRE_LIST = 'https://api.themoviedb.org/3/genre/movie/list';
+const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const API_URL_MOVIE_LIST = `${BASE_URL}/movie/now_playing`;
+export const API_URL_GENRE_LIST = `${BASE_URL}/genre/movie/list`;
 
 export const URL_MOVIE_IMAGE = 'https://image.tmdb.org/t/p/w500';
 
-export const URL_MOVIE_SEARCH = 'https://api.themoviedb.org/3/search/movie';
+export const URL_MOVIE_SEARCH = `${BASE_URL}/search/movie`;
 
-export const URL_MOVIE = 'https://api.themoviedb.org/3/movie';
+export const URL_MOVIE = `${BASE_URL}/movie`;
 
 export const LANGUAGE_CODE = 'en-US';
 
